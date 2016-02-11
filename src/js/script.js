c0ac89b1c8af8a4c474d98bab5f935982ddd723a
William Chong <williamc@tribalworldwide.ca>
2016-02-11 00:12:27 -0500

    designs and stuff

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,35 @@
 $(document).ready(function() {
 
-	var resizedHeight = $('.video').width();
-	resizedHeight = resizedHeight*(635/992);
-	$('.video').css('height', resizedHeight);
+// 	var resizedHeight = $('.video').width();
+// 	resizedHeight = resizedHeight*(635/992);
+// 	$('.video').css('height', resizedHeight);
 
-	var resizedWidth = $('.video').width();
-	resizedWidth = resizedWidth*(703/992);
-	$('.video-footer-container').css('width', resizedWidth);
+// 	var resizedWidth = $('.video').width();
+// 	resizedWidth = resizedWidth*(703/992);
+// 	$('.video-footer-container').css('width', resizedWidth);
+	
+	
+	$(".nav-button").click(function(e) {
+	    e.preventDefault();
+	    var p = $(this).parent();
+	    if (p.attr('href') == "rules.pdf") {
+	        window.open('rules.pdf', '_blank');
+	    } else {
+	        window.location.href = p.attr('href');
+	    }
+	});
 
 });
 
-$(window).on('resize', function() {
+// $(window).on('resize', function() {
 
-	var resizedHeight = $('.video').width();
-	resizedHeight = resizedHeight*(635/992);
-	$('.video').css('height', resizedHeight);
+// 	var resizedHeight = $('.video').width();
+// 	resizedHeight = resizedHeight*(635/992);
+// 	$('.video').css('height', resizedHeight);
 
-	var resizedWidth = $('.video').width();
-	resizedWidth = resizedWidth*(703/992);
-	$('.video-footer-container').css('width', resizedWidth);
+// 	var resizedWidth = $('.video').width();
+// 	resizedWidth = resizedWidth*(703/992);
+// 	$('.video-footer-container').css('width', resizedWidth);
 
-});
+// });
+
